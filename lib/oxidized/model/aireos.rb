66c8cad938efa8a2c165ae27c0dfaf3a1a8160e2
Alex Passfall <alex.passfall@eternalnet.ch>
2022-08-09 13:37:16 +0200

    aireos fix

--- lib/oxidized/model/aireos.rb
+++ lib/oxidized/model/aireos.rb
@@ -2,7 +2,7 @@ class Aireos < Oxidized::Model
   # AireOS (at least I think that is what it's called, hard to find data)
   # Used in Cisco WLC 5500
 
-  comment '# ' # this complains too, can't find real comment char
+  comment '! '
   prompt /^\([^)]+\)\s>/
 
   cmd :all do |cfg|
@@ -41,9 +41,19 @@ class Aireos < Oxidized::Model
 
   def clean(cfg)
     out = []
+    cfg = cfg.gsub(/\s{20,}/,"\n")
     cfg.each_line do |line|
+      raise StandardError if line =~ /.*There is another transfer activity going on.*$/
       next if line =~ /^\s*$/
       next if line =~ /rogue (adhoc|client) (alert|Unknown) [\da-f]{2}:/
+      next if line =~ /Config generation may take some time.*$/
+      next if line =~ /Cannot execute command.*$/
+      next if line =~ /Please ignore messages.*$/
+      next if line =~ /transfer (upload|download).*$/
+      next if line =~ /.*WLC Config (Begin|End).*$/
+      next if line =~ /.*Blocked: Configuration is blocked.*$/
+      next if line[0] == "#"
+      next if line[0] == "!"
 
       line = line[1..-1] if line[0] == "\r"
       out << line.strip
